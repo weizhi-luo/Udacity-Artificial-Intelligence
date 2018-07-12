ba94a890046440412f552c06074b893f7b667f4a
weizhi-luo <weizhi.luo@googlemail.com>
2018-07-13 00:31:05 +0100

    Partial

--- Projects/aind2-rnn/my_answers.py
+++ Projects/aind2-rnn/my_answers.py
@@ -32,18 +32,23 @@ def build_part1_RNN(window_size):
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
     punctuation = ['!', ',', '.', ':', ';', '?']
-
-    return text
+    punctuation_set = set(punctuation)
+    
+    return ''.join([c for c in text if c not in punctuation_set])
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_text(text, window_size, step_size):
     # containers for input/output pairs
-    inputs = []
-    outputs = []
+    inputs = [text[i:i+window_size] for i in range(0, len(text), step_size) if len(text[i:i+window_size]) == window_size]
+    outputs = [text[i] for i in range(window_size, len(text), step_size)]
 
     return inputs,outputs
 
 # TODO build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
-    pass
+    model = Sequential()
+    model.add(LSTM(200, input_shape = (window_size, num_chars)))
+    model.add(Dense(num_chars, activation = 'softmax'))
+
+    return model
